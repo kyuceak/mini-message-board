3e9e82040e69dd9281843963b84de45b915f3199
kyuceak <kyuceak@sabanciuniv.edu>
2025-02-26 02:58:05 +0300

    minor bug fix date

--- db/queries.js
+++ db/queries.js
@@ -5,12 +5,12 @@ const pool = require("./pool");
 
 
 async function getAllMessages(){
-    const { rows } = await pool.query("SELECT * FROM messages");
+    const { rows } = await pool.query("SELECT text,username,to_char(added, 'Mon DD YYYY HH24:MI:SS') AS formatted_added FROM messages");
     return rows;
 }
 
 async function getMessage(id){
-    const {rows} = await pool.query("SELECT * FROM messages WHERE messages.id = $1",[id]);
+    const {rows} = await pool.query("SELECT text,username,to_char(added, 'Mon DD YYYY HH24:MI:SS') AS formatted_added FROM messages WHERE messages.id = $1",[id]);
     
     return rows[0];
 }
